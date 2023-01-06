935d8d8bf319db6f879e2d4ae91658fdfb68e849
KimEungChan <dmdcks789@gmail.com>
2023-01-06 17:44:31 +0900

    Redux-toolkit Practice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrease } from './pages/counterSlice';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 200px;
+  height: 200px;
+  font-size: 100px;
+`
 
 function App() {
+  const count = useSelector((state)=>state.counter.value);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <div>
+        {count}
+      </div>
+      <Button onClick={()=>{dispatch(increment())}}>+</Button>
+      <Button onClick={()=>{dispatch(decrease())}}>-</Button>
+    </>
   );
 }
 
