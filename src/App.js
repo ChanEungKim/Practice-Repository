13e179b6f0a2f4aa05f2aad148746965268ad482
ChanEungKim <dmdcks000@naver.com>
2023-02-02 17:08:46 +0900

    react-router practice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./component/ScrollToTop";
 import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Page1 />}></Route>
+          <Route path="/page2/:pageId" element={<Page2 />}></Route>
         </Routes>
       </BrowserRouter>
     </>
